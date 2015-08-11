f295119e2dba687080db60c22d0cf6fb24eeec65
Kyle Lucas <kglucas93@gmail.com>
2015-08-11 09:47:28 -0700

    Update bronto.rb

--- lib/bronto.rb
+++ lib/bronto.rb
@@ -12,7 +12,7 @@ require_relative "bronto/version"
 
 require_relative "core_ext/array"
 require_relative "core_ext/object"
-require_relative "core_ext/string"
+
 
 module Bronto
   class Error < StandardError
